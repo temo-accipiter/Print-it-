de7ac8aeec639a4131d1244af0be5d962859688f
timothée <temurimi@gmail.com>
2023-05-11 09:58:47 +0200

    add a part of js code for slider

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,42 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const mainImg = document.querySelector('.banner-img')
+const mainTagline = document.querySelector('.main_tagline')
+const dots = document.querySelectorAll('.dot');
+
+document.querySelector('.arrow_right').addEventListener('click', arrowRight)
+
+let currentSlideIndex = 0;
+
+function arrowRight() {
+	currentSlideIndex++;
+	mainImg.src = slides[currentSlideIndex].image
+	mainTagline.innerHTML = slides[currentSlideIndex].tagLine;
+	dots[currentSlideIndex].classList.add('dot_selected');
+}
+
+/* Left Arrow */
+document.querySelector('.arrow_left').addEventListener('click', arrowLeft)
+function arrowLeft() {
+	currentSlideIndex--;
+	mainImg.src = slides[currentSlideIndex].image;
+	mainTagline.innerHTML = slides[currentSlideIndex].tagLine;
+	dots[currentSlideIndex].classList.add('dot_selected');
+}
